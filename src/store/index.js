6ce1fa18841a991f5f7f45e787bb345d0d7b77de
kscheltema <k87scheltema@outlook.com>
2021-06-20 13:17:37 +0200

    createSlice a way to manage multiple state

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,29 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { count: 0, showCount: false };
-//all state values need to be passed down to the other components
-//the variation**    state.counter++   ** would be classified as state mutation
-//thus always return an new state that is over written
+const counterSlice = createSlice({
+  name: "counter", //a required property
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++; //state mutation is allowed here
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    decrease(state, action) {
+      state.counter = state.counter - action.amount;
+    },
+    toggle(state) {
+      state.showCount = !state.showCount;
+    },
+  },
+});
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return { count: state.count + 1, showCount: state.showCount };
-  }
-  if (action.type === "decrement") {
-    return { count: state.count - 1, showCount: state.showCount };
-  }
-  if (action.type === "increase") {
-    return { count: state.count + action.amount, showCount: state.showCount };
-  }
-  if (action.type === "decrease") {
-    return { count: state.count - action.amount, showCount: state.showCount };
-  }
-  if (action.type === "toggle") {
-    return { count: state.count, showCount: !state.showCount };
-  }
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = createStore(counterSlice.reducer);
 export default store;
+//one method to use createSlice
